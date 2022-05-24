b35fff738a49cfd45e0b8fe43f5125b1b0178d9d
Eric_the_human <104437044+Eric-gottem-apple-bottom-jeans@users.noreply.github.com>
2022-05-24 00:26:14 +0000

    in part 3 I used if and elif along with comparison signs to put conditions on numbers, and I put the percentage part == 0 to show if that number was evenly divisible by 3 or not

--- part3.py
+++ part3.py
@@ -2,7 +2,7 @@
 ______
 PART 3
 ______
-Write a program that asks the user to input one integer. The program will then print two strings, one stating if it's positive, negative, or zero, and another that states whether or not is is divisible by 3. (Hint: to check divisibility by 3, you will find using the modulus(%) operation very helpful.)
+Write a program  that asks the user to input one integer. The program will then print two strings, one stating if it's positive, negative, or zero, and another that states whether or not is is divisible by 3. (Hint: to check divisibility by 3, you will find using the modulus(%) operation very helpful.)
 
 3 examples of what should appear on the console when the program runs (note: the test driver is case sensitive):
 
